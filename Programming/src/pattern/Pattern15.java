d617f6769f31b0b14726c6d1bd34d2f93fa56d9d
HsAdmin <HsAdmin@HS14049050>
2021-04-22 22:47:22 +0530

    test

--- Programming/src/pattern/Pattern15.java
+++ Programming/src/pattern/Pattern15.java
@@ -1,9 +1,58 @@
+
+
+/*
+ 
+   1
+   12
+  123
+ 1234
+12345
+ 1234
+  123
+   12
+    1
+ 
+  
+ */
+
+
+
 package pattern;
+
+import java.util.Scanner;
 
 public class Pattern15 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int k=0;
+
+		for (int i = 1; i <= 2*n-1; i++) {
+			int p=1;
+			if(i<=5) {
+				k++;
+			}
+			else {
+				k--;
+			}
+
+			for (int j = 1; j <=n; j++) {
+
+				if (j >=6-k) {
+					System.out.print(p);
+					p++;
+					
+
+				} else {
+					System.out.print(" ");
+				}
+
+			}
+			System.out.println();
+		}
+
 
 	}
 
